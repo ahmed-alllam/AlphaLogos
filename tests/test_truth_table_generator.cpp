5e61ba7209d3584d60a89fb26528a9dc883b29b8
Ahmed Allam <ahmed39652003@gmail.com>
2023-10-20 19:39:37 +0300

    Added 4 more test cases. Fixed CMake error

--- tests/test_truth_table_generator.cpp
+++ tests/test_truth_table_generator.cpp
@@ -189,3 +189,57 @@ TEST_CASE(
 
   REQUIRE(validateTruthTable(expected, expr) == true);
 }
+
+TEST_CASE("Truth table for expression a'bc + ab' + bc'") {
+  string expr = "a'bc + ab' + bc'";
+
+  std::unordered_map<std::vector<bool>, bool> expected = {
+      {{true, true, true}, false},   {{true, true, false}, true},
+      {{true, false, true}, true},   {{true, false, false}, true},
+      {{false, true, true}, true},   {{false, true, false}, true},
+      {{false, false, true}, false}, {{false, false, false}, false}};
+
+  REQUIRE(validateTruthTable(expected, expr) == true);
+}
+
+TEST_CASE("Truth table for expression cd+b'c+ a d") {
+  string expr = "cd+b'c+ a d";
+
+  std::unordered_map<std::vector<bool>, bool> expected = {
+      {{true, true, true, true}, true},
+      {{true, true, true, false}, true},
+      {{true, true, false, true}, true},
+      {{true, true, false, false}, true},
+      {{true, false, true, true}, false},
+      {{true, false, true, false}, false},
+      {{true, false, false, true}, true},
+      {{true, false, false, false}, true},
+      {{false, true, true, true}, true},
+      {{false, true, true, false}, false},
+      {{false, true, false, true}, true},
+      {{false, true, false, false}, false},
+      {{false, false, true, true}, false},
+      {{false, false, true, false}, false},
+      {{false, false, false, true}, false},
+      {{false, false, false, false}, false}};
+
+  REQUIRE(validateTruthTable(expected, expr) == true);
+}
+
+TEST_CASE("Truth table for expression aa'") {
+  string expr = "aa'";
+
+  std::unordered_map<std::vector<bool>, bool> expected = {{{true}, false},
+                                                          {{false}, false}};
+
+  REQUIRE(validateTruthTable(expected, expr) == true);
+}
+
+TEST_CASE("Truth table for expression a + a'") {
+  string expr = "a + a'";
+
+  std::unordered_map<std::vector<bool>, bool> expected = {{{true}, true},
+                                                          {{false}, true}};
+
+  REQUIRE(validateTruthTable(expected, expr) == true);
+}
