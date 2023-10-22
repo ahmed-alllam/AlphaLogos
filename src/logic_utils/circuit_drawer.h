99717929720a1a2ff7b6ed9c8b211b184f801b79
Ahmed Allam <ahmed39652003@gmail.com>
2023-10-22 21:42:39 +0300

    Added circuit drawing feature

---
+++ src/logic_utils/circuit_drawer.h
@@ -0,0 +1,13 @@
+#ifndef CIRCUIT_DRAWER_H
+#define CIRCUIT_DRAWER_H
+
+#include <iostream>
+#include <string>
+
+#include "truth_table_generator.h"
+
+using namespace std;
+
+string SoPToWaveJSON(vector<Token> tokens);
+
+#endif  // CIRCUIT_DRAWER_H
