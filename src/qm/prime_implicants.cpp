df0d3b9402d363aedf2c747a9df40666e5223fec
Ahmed Allam <ahmed39652003@gmail.com>
2023-10-22 15:42:33 +0300

    Added generating all prime implicants (PIs) of a function using Quine-McCluskey Logic Minimization algorithm

---
+++ src/qm/prime_implicants.cpp
@@ -0,0 +1,67 @@
+#include "prime_implicants.h"
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+#include "../logic_utils/truth_table_generator.h"
+
+using namespace std;
+
+vector<Implicant> generatePrimeImplicants(vector<Minterm> minterms) {
+  vector<Implicant> prime_implicants;
+  vector<Implicant> column;
+
+  for (const auto& minterm : minterms) {
+    column.push_back({{minterm.index}, minterm.binary, false, false, false});
+  }
+
+  while (!column.empty()) {
+    vector<Implicant> nextColumn;
+    for (int i = 0; i < column.size(); i++) {
+      for (int j = i + 1; j < column.size(); j++) {
+        int num_diff = 0;
+        int diff_index = -1;
+        for (int k = 0; k < column[i].binary.size(); k++) {
+          if (column[i].binary[k] != column[j].binary[k]) {
+            num_diff++;
+            diff_index = k;
+          }
+        }
+        if (num_diff == 1) {
+          vector<int> new_minterms(column[i].minterms);
+          new_minterms.insert(new_minterms.end(), column[j].minterms.begin(),
+                              column[j].minterms.end());
+          vector<int> new_binary = column[i].binary;
+          new_binary[diff_index] = -1;  // `-1` means `-`
+          nextColumn.push_back({new_minterms, new_binary, false, false, false});
+          column[i].is_checked = true;
+          column[j].is_checked = true;
+        }
+      }
+    }
+    for (const auto& implicant : column) {
+      if (!implicant.is_checked) {
+        prime_implicants.push_back(implicant);
+      }
+    }
+    column.swap(nextColumn);
+  }
+
+  return prime_implicants;
+}
+
+string implicantToString(const Implicant& implicant,
+                         vector<Token> variableTokens) {
+  string result;
+
+  for (int i = 0; i < implicant.binary.size(); i++) {
+    if (implicant.binary[i] == 1) {
+      result += variableTokens[i].value;
+    } else if (implicant.binary[i] == 0) {
+      result += variableTokens[i].value;
+      result += '\'';
+    }
+  }
+  return result;
+}
--- tests/test_add.cpp
+++
@@ -1,6 +0,0 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch_test_macros.hpp>
-
-#include "../src/logic_utils/add.h"
-
-TEST_CASE("Addition of 1, and 2") { REQUIRE(add(1, 2) == 3); }
