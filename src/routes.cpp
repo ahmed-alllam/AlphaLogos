b0e294f2f69126695eb11e831670607d459d01ca
Ahmed Allam <ahmed39652003@gmail.com>
2023-10-24 20:56:47 +0300

    Added crow handlers for validation. Added script.js with validate function

--- src/routes.cpp
+++ src/routes.cpp
@@ -1,10 +1,18 @@
 #include "routes.h"
 
 #include "handlers/main_handler.h"
+#include "handlers/validation_handler.h"
 
 void setup_routers(crow::SimpleApp& app) {
   CROW_ROUTE(app, "/").methods(crow::HTTPMethod::Get)(
       [](const crow::request& req, crow::response& res) {
         main_handler(req, res);
       });
+
+  // route for validation
+  CROW_ROUTE(app, "/validate")
+      .methods(crow::HTTPMethod::Post)(
+          [](const crow::request& req, crow::response& res) {
+            validation_handler(req, res);
+          });
 }
