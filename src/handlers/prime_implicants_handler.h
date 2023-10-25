551a299aee510394b27640ad54dd0c45f1d8fb4a
Ahmed Allam <ahmed39652003@gmail.com>
2023-10-25 22:05:58 +0300

    Added Prime Implicants functionality to the webpage of the project.

---
+++ src/handlers/prime_implicants_handler.h
@@ -0,0 +1,8 @@
+#ifndef PRIME_IMPLICANTS_HANDLER_H
+#define PRIME_IMPLICANTS_HANDLER_H
+
+#include <crow.h>
+
+void prime_implicants_handler(const crow::request& req, crow::response& res);
+
+#endif  // PRIME_IMPLICANTS_HANDLER_H
