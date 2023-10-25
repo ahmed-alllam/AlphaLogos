99eb395524103f9900c91d715aee34b1200520ab
Ahmed Allam <ahmed39652003@gmail.com>
2023-10-25 14:20:00 +0300

    Added truth table handlers. Added Jinja Template for truth table.
    Added better styles for webpage.
    
    Added the logic in JavaScript to handle truth table calls.

--- src/routes.cpp
+++ src/routes.cpp
@@ -1,18 +1,25 @@
 #include "routes.h"
 
 #include "handlers/main_handler.h"
+#include "handlers/truth_table_handler.h"
 #include "handlers/validation_handler.h"
 
-void setup_routers(crow::SimpleApp& app) {
+void setup_routers(crow::SimpleApp &app) {
   CROW_ROUTE(app, "/").methods(crow::HTTPMethod::Get)(
-      [](const crow::request& req, crow::response& res) {
+      [](const crow::request &req, crow::response &res) {
         main_handler(req, res);
       });
 
   // route for validation
   CROW_ROUTE(app, "/validate")
       .methods(crow::HTTPMethod::Post)(
-          [](const crow::request& req, crow::response& res) {
+          [](const crow::request &req, crow::response &res) {
             validation_handler(req, res);
+          });
+
+  CROW_ROUTE(app, "/truth-table")
+      .methods(crow::HTTPMethod::Post)(
+          [](const crow::request &req, crow::response &res) {
+            truth_table_handler(req, res);
           });
 }
