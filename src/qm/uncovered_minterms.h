7b38e6254bbb3378e82d6c42222dd4653789f35d
Mohamed Abdelmagid <68608858+MohamedEbrahem1@users.noreply.github.com>
2023-10-27 22:15:46 +0200

    Fixed some errors

--- src/qm/uncovered_minterms.h
+++ src/qm/uncovered_minterms.h
@@ -1,7 +1,7 @@
 #ifndef UNCOVERED_MINTERMS_H
 #define UNCOVERED_MINTERMS_H
+#include "../src/logic_utils/minterm.h"
 #include "implicant.h"
-#include "minterm.h"
 vector<int> getUncoveredMinterms(vector<Minterm>& minterms,
                                  const vector<Implicant>& essentialImplicants);
 
