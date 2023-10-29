4b1f17012db2991d82d4e341c419dc6a61a22d09
Mohamed Abdelmagid <68608858+MohamedEbrahem1@users.noreply.github.com>
2023-10-30 00:40:32 +0200

    Added a function to return the set of minimized implicants
    Updated petrick.h file

--- src/qm/petrick.h
+++ src/qm/petrick.h
@@ -1,13 +1,38 @@
 #ifndef PETRICK_H
 #define PETRICK_H
 
-#include <algorithm>
+// QuineMcCluskeySolver.h
+
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
+#include "../logic_utils/minterm.h"
 #include "implicant.h"
 
-using namespace std;
+class QuineMcCluskeySolver {
+ public:
+  static std::vector<Implicant> removeImplicants(
+      const std::vector<Implicant>& source,
+      const std::vector<Implicant>& implicantsToRemove);
 
-vector<Implicant> removeDominatedRows(const std::vector<Implicant>& implicants);
+  static bool compareImplicants(const Implicant& a, const Implicant& b);
 
-#endif  // IMPLICANT_UTILS_H
+  static std::vector<Implicant> generateEssentialPrimeImplicants(
+      const std::vector<Implicant>& primeImplicants);
+
+  static std::vector<Implicant> removeDuplicateImplicants(
+      const std::vector<Implicant>& implicants);
+
+  static std::vector<Implicant> removeDominatedRows(
+      const std::vector<Implicant>& implicants);
+
+  static std::vector<Implicant> petrick(
+      const std::vector<Implicant>& primeImplicant);
+
+  // Assuming other functions like removeMinterms, getUniqueMinterms, etc. are
+  // also public, add their declarations here.
+};
+
+#endif  // QUINE_MCCLUSKEY_SOLVER_H
