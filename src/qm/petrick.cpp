0ac347b2b432e9b542fcd8177e5d70eea9db1f7e
Ahmed Allam <ahmed39652003@gmail.com>
2023-10-30 14:31:56 +0200

    Solved Bug in Petrick causing infinite loop

--- src/qm/petrick.cpp
+++ src/qm/petrick.cpp
@@ -12,20 +12,20 @@ using namespace std;
 #include "petrick.h"
 
 vector<Implicant> removeImplicants(
-    const vector<Implicant>& source,
-    const vector<Implicant>& implicantsToRemove) {
+    const vector<Implicant> &source,
+    const vector<Implicant> &implicantsToRemove) {
   vector<Implicant> result = source;
-  for (const auto& toRemove : implicantsToRemove) {
+  for (const auto &toRemove : implicantsToRemove) {
     result.erase(remove(result.begin(), result.end(), toRemove), result.end());
   }
   return result;
 }
 
 vector<Implicant> removeDuplicateImplicants(
-    const vector<Implicant>& implicants) {
+    const vector<Implicant> &implicants) {
   vector<Implicant> result;
 
-  for (const Implicant& implicant : implicants) {
+  for (const Implicant &implicant : implicants) {
     if (find(result.begin(), result.end(), implicant) == result.end()) {
       result.push_back(implicant);
     }
@@ -34,7 +34,7 @@ vector<Implicant> removeDuplicateImplicants(
   return result;
 }
 
-vector<Implicant> removeDominatedRows(const vector<Implicant>& implicants) {
+vector<Implicant> removeDominatedRows(const vector<Implicant> &implicants) {
   vector<Implicant> result;
 
   for (size_t i = 0; i < implicants.size(); ++i) {
@@ -57,23 +57,23 @@ vector<Implicant> removeDominatedRows(const vector<Implicant>& implicants) {
 }
 
 vector<pair<int, vector<vector<int>>>> associatePrimeImplicants(
-    const vector<Implicant>& implicants) {
+    const vector<Implicant> &implicants) {
   vector<pair<int, vector<vector<int>>>> result;
 
   // Create a map to store the prime implicants associated with each minterm
   unordered_map<int, vector<vector<int>>> mintermPrimeImplicants;
 
   // Collect all minterms from implicants and associate prime implicants
-  for (const Implicant& implicant : implicants) {
+  for (const Implicant &implicant : implicants) {
     for (int minterm : implicant.minterms) {
       mintermPrimeImplicants[minterm].push_back(implicant.binary);
     }
   }
 
   // Create the result vector with minterm index and associated prime implicants
-  for (const auto& entry : mintermPrimeImplicants) {
+  for (const auto &entry : mintermPrimeImplicants) {
     int minterm = entry.first;
-    const vector<vector<int>>& primeImplicants = entry.second;
+    const vector<vector<int>> &primeImplicants = entry.second;
 
     result.emplace_back(minterm, primeImplicants);
   }
@@ -82,26 +82,26 @@ vector<pair<int, vector<vector<int>>>> associatePrimeImplicants(
 }
 
 vector<int> findDominatingMinterms(
-    const vector<pair<int, vector<vector<int>>>>& mintermPrimeImplicants) {
+    const vector<pair<int, vector<vector<int>>>> &mintermPrimeImplicants) {
   unordered_set<int> dominatingMinterms;
 
   // Check for dominating minterms
   for (auto it1 = mintermPrimeImplicants.begin();
        it1 != mintermPrimeImplicants.end(); ++it1) {
     int minterm1 = it1->first;
-    const vector<vector<int>>& primeImplicants1 = it1->second;
+    const vector<vector<int>> &primeImplicants1 = it1->second;
 
     for (auto it2 = next(it1); it2 != mintermPrimeImplicants.end(); ++it2) {
       int minterm2 = it2->first;
-      const vector<vector<int>>& primeImplicants2 = it2->second;
+      const vector<vector<int>> &primeImplicants2 = it2->second;
 
       bool isDominating = true;
 
       // Check if minterm1 dominates minterm2
-      for (const vector<int>& implicant2 : primeImplicants2) {
+      for (const vector<int> &implicant2 : primeImplicants2) {
         bool isCovered = false;
 
-        for (const vector<int>& implicant1 : primeImplicants1) {
+        for (const vector<int> &implicant1 : primeImplicants1) {
           if (equal(implicant2.begin(), implicant2.end(), implicant1.begin())) {
             isCovered = true;
             break;
@@ -124,13 +124,13 @@ vector<int> findDominatingMinterms(
   return vector<int>(dominatingMinterms.begin(), dominatingMinterms.end());
 }
 
-vector<Implicant> removeDominatingMinterms(vector<Implicant>& implicants,
-                                           const vector<int>& minterms) {
+vector<Implicant> removeDominatingMinterms(vector<Implicant> &implicants,
+                                           const vector<int> &minterms) {
   vector<Implicant> filteredImplicants;
 
-  for (const auto& implicant : implicants) {
+  for (const auto &implicant : implicants) {
     vector<int> remainingMinterms;
-    for (const auto& m : implicant.minterms) {
+    for (const auto &m : implicant.minterms) {
       if (!binary_search(minterms.begin(), minterms.end(), m)) {
         remainingMinterms.push_back(m);
       }
@@ -143,11 +143,11 @@ vector<Implicant> removeDominatingMinterms(vector<Implicant>& implicants,
   return filteredImplicants;
 }
 
-vector<int> getUniqueMinterms(vector<Implicant>& implicants) {
+vector<int> getUniqueMinterms(vector<Implicant> &implicants) {
   unordered_set<int> uniqueMintermsSet;
 
   // Iterate over each implicant
-  for (const auto& implicant : implicants) {
+  for (const auto &implicant : implicants) {
     // Add minterms to the set to ensure uniqueness
     for (int minterm : implicant.minterms) {
       uniqueMintermsSet.insert(minterm);
@@ -161,14 +161,14 @@ vector<int> getUniqueMinterms(vector<Implicant>& implicants) {
   return uniqueMinterms;
 }
 
-vector<Implicant> removeMinterms(vector<Implicant>& implicants,
-                                 const vector<int>& mintermsToRemove) {
-  for (auto& implicant : implicants) {
+vector<Implicant> removeMinterms(vector<Implicant> &implicants,
+                                 const vector<int> &mintermsToRemove) {
+  for (auto &implicant : implicants) {
     // Create a copy of the implicant's minterms
     vector<int> updatedMinterms = implicant.minterms;
 
     // Iterate over the minterms to remove
-    for (const auto& mintermToRemove : mintermsToRemove) {
+    for (const auto &mintermToRemove : mintermsToRemove) {
       // Find and remove the minterm from the updated minterms
       auto it =
           find(updatedMinterms.begin(), updatedMinterms.end(), mintermToRemove);
@@ -184,12 +184,24 @@ vector<Implicant> removeMinterms(vector<Implicant>& implicants,
   return implicants;
 }
 
-vector<Implicant> petrick(const vector<Implicant>& primeImplicant) {
+vector<Implicant> removeEmptyImplicants(const vector<Implicant> &implicants) {
+  vector<Implicant> result;
+
+  for (const Implicant &implicant : implicants) {
+    if (!implicant.minterms.empty()) {
+      result.push_back(implicant);
+    }
+  }
+
+  return result;
+}
+
+vector<Implicant> petrick(const vector<Implicant> &primeImplicant) {
   vector<pair<Implicant, vector<int>>> primeImplicantMinterms;
   vector<Implicant> copy_implicants = primeImplicant;
   vector<Implicant> solution;
 
-  for (const auto& implicant : primeImplicant) {
+  for (const auto &implicant : primeImplicant) {
     primeImplicantMinterms.emplace_back(implicant, implicant.minterms);
   }
 
@@ -206,12 +218,13 @@ vector<Implicant> petrick(const vector<Implicant>& primeImplicant) {
     copy_implicants = removeDominatingMinterms(
         copy_implicants,
         findDominatingMinterms(associatePrimeImplicants(copy_implicants)));
+    copy_implicants = removeEmptyImplicants(copy_implicants);
   }
 
   solution = removeDuplicateImplicants(solution);
 
-  for (auto& implicant : solution) {
-    for (auto& original_implicant : primeImplicantMinterms) {
+  for (auto &implicant : solution) {
+    for (auto &original_implicant : primeImplicantMinterms) {
       if (implicant.binary == original_implicant.first.binary) {
         implicant.minterms = original_implicant.second;
       }
